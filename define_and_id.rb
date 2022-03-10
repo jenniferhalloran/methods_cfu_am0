c3aac6b71bfef863c3e2b81665917cbd7d933948
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-03-10 09:37:29 -0500

    Add completed define_and_id.rb

--- define_and_id.rb
+++ define_and_id.rb
@@ -7,17 +7,27 @@
 # The return value is "HI"
  "Hello World".upcase
 
-
+# The include? method is called on the string "Hello World"
+# The argument "Hello" is passed; we are asking if the String includes our argument ("Hello").
+# The return value is a Boolean - true.
 p "Hello World".include?("Hello")
 
-
+# The end_with? method is called on the string "Hello World"
+# The argument "Hello" is passed; we are asking if the String ends with the characters in our argument "Hello"
+# The return value is a Boolean - false
 p "Hello World".end_with?("Hello")
 
-
+# The end_with? method is called on the string "Hello World" again
+# This time the argument "rld" is passed; we are asking if the String ends with the characters in our argument, "rld"
+# The return value is a Boolean - true
 p "Hello World".end_with?("rld")
 
-
+# The .length method is called on the string "Hello World"
+# This method does not require an argument; we are just asking the length of the given String
+# The return value is 11 - because there are 11 characters including the space.
 p "Hello World".length
 
-
+# The .size method is called on the string "Hello World"
+# This method does not require/have an argument; from my understanding we are asking to count the number of elements along a given axis.
+# It seems there is only one axis here, so it returns the size of the string which is 11 elements again.
 p "Hello World".size
