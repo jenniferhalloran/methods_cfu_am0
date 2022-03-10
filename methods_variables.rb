60bca61ebab98c76bc816f05d26b5fde6e4e58c2
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-03-10 09:49:06 -0500

    Add part 1 of methods_variables.rb

--- methods_variables.rb
+++ methods_variables.rb
@@ -2,12 +2,16 @@
 # PART 1
 
 # Define a variable that stores a string
+name = "Jennifer Lauren Halloran"
 
 #  call upcase on the variable, print it out
+p name.upcase
 #  call downcase on the variable, print it out
+p name.downcase
 #  call reverse on the variable, print it out
+p name.reverse
 #  call lenght on the variable, print it out
-
+p name.length
 
 
 
@@ -20,13 +24,9 @@
 user_name = "coco_11am"
 last_login = "12/09/2021"
 
-# 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do. 
+# 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do.
   # Write the methods out on the lines below, with an explanation in your own words of how they work.
 
 
 
 # 2: Do some research (either testing out with your own code or Googling) to build an understanding of what the `!` does, when at the end of a method name. Show your understading by providing an example and writing an explanation.
-
-
-
-
